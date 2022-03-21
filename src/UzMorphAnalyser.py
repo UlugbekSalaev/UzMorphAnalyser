d8470c52f8e31632889a1eaee336c5536bbc0268
Ulugbek Salaev <ulugbek0302@gmail.com>
2022-03-21 21:57:57 +0500

    Completed stem method

--- src/UzMorphAnalyser.py
+++ src/UzMorphAnalyser.py
@@ -3,22 +3,39 @@
 import csv
 
 class UzMorphAnalyser:
-    __data = [] #list of affixes table
+    __affixes = [] #list of affixes table from affixes.csv file
+    __small_words = [] #list of small words from small_words.csv file
+    __non_affixed_words = []  # list of non affixed words from non_affixed_words.csv file
+    __exception_words = []  # list of exception words from exception_words.csv file
 
     def __init__(self):
         self.__read_data()
 
     def __read_data(self):
-        with open("data.csv", "r") as f:
+        with open("affixes.csv", "r") as f:
             reader = csv.DictReader(f)
-            self.__data = list(reader)
+            self.__affixes = list(reader)
+        with open("small_words.csv", "r") as f:
+            reader = csv.reader(f)
+            #self.__small_words = list(reader)
+            self.__small_words = [item for sublist in list(reader) for item in sublist]
+        with open("non_affixed_words.csv", "r") as f:
+            reader = csv.reader(f)
+            #self.__small_words = list(reader)
+            self.__non_affixed_words = [item for sublist in list(reader) for item in sublist]
+        with open("exception_words.csv", "r") as f:
+            reader = csv.DictReader(f)
+            self.__exception_words = list(reader)
+    #enf of read_data
 
+    #affixes.csv da barcha allomorphlarni qulda generate qilib yozib quyamiz, dastur yordamida qilmaymiz, chalkash joylari kup
+    #bu generate funksiya faqat boshda qavsli turganiga va boshda turgan katta harfliga tugri keladi.
     def __GeneratedAllomorph(self, affix): #return a list that contain all allomorphs of the current affix
         GenAff=[]
-        #if allomorph has omitted letter
+        #if allomorph has omitted letter # qavsli faqat affix boshida keladi
         if affix[0]=="(":
-            GenAff.append( affix[1]+affix[3:] ) #(affix.replace("(","")).replace(")","")
-            GenAff.append( affix[3:] )
+            GenAff.append( affix.replace("(", "").replace(")","")  ) #affix[1]+affix[3:] #qavsdagi bilan olish
+            GenAff.append( affix[affix.find(")")+1:] ) #qavsdan keyingilarini olish
             return GenAff
         #if allomorph has uppper letter (several letters)
         if (affix[0]).isupper():
@@ -41,27 +58,45 @@ class UzMorphAnalyser:
                 GenAff.append("k" + affix[1:])
                 GenAff.append("q" + affix[1:])
             return GenAff
-        return affix #if the affix does't have allomorph then return itself
+        GenAff.append(affix)
+        return GenAff #if the affix does't have allomorph then return itself
+    #end of Generate
 
     def stem(self, word: str):
-        #root=word[:1]
-        #affix=word[1:]
-        #print(root, ' ', affix)
-        #row=next((item for item in data if item["affix"] == affix), None)
-        #if row==None:
-        #    print(row)
-        #else: print(row["affix"])
 
-        size=len(word)
-        for i in range(1, size):
-            affix = word[i:]
-            for item in self.__data:
-                if(affix in self.__GeneratedAllomorph(item["affix"])):
-                    return word[:i]
-        return word
+        def stem_find(self, word:str, position:int=2):
+            for i in range(position, len(word)):
+                #predict_as_affix = word[i:]
+                for item in self.__affixes:
+                    if word[i:] in self.__GeneratedAllomorph(item["affix"]):
+                        #print(position)
+                        #print(self.__GeneratedAllomorph(item["affix"]))
+                        #print(word[i:])
+                        #print(item["affix"])
+                        #print(self.__exception_words)
+                        #print(item["confidence"])
+                        if float(item["confidence"]) < 0.3:
+                            if word in [ex_word['word'] for ex_word in self.__exception_words]:
+                                return word
+                        return word[:i]
+            return word
+        #end of stem_find
+
+        #algorithm
+        #1. check non affixed words list
+        if word in self.__non_affixed_words:
+            return word
+
+        #2. find stem by affix checking from affixes list
+        stem = stem_find(self, word)
+        if len(stem)<=2:
+            if not stem in self.__small_words:
+                stem=stem_find(self, word, 3)
+
+        return stem
+        #end of stem
 
     def lemma(self, word: str, POS: str="n"):
-
         return word
 
     def lemmatize(self, word: str, POS: str = "n"):
@@ -84,7 +119,9 @@ class UzMorphAnalyser:
         return tokens
 
 obj = UzMorphAnalyser()
-print(obj.stem("kelayotganlaringizda"))
+sent = "kitob daftar"
+for token in sent.split(" "):
+    print(obj.stem(token))
 
 #print(UzMorphAnalyser.stem("meniki"))
 
